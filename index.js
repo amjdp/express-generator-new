b4f48996a338dff34459005f2f1d651c5b488ec5
amjdp <athiramj123@gmail.com>
2023-03-04 11:54:10 +0530

    march 04 version

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ const app = express();
 // });
 
 // app.use(`${config.api.prefix}`,logMiddlewre.logRequest, route);
+console.log(config.api.prefix);
 
-app.use(`${config.api.prefix}`,()=>{
+app.get(`/${config.api.prefix}`,(req,res)=>{
     console.log("hello");
+    res.status(400).json({message:"hello"});
 });
+
+// app.use(`${config.api.prefix}`,(req,res)=>{
+//     responseHelper(res, { statusCode: 404, message: "Route not found", data: [], error: []});
+// });
 
 // api/prefix - log when api hits - middleware - actual route
 // config - diff environments - diff urls
